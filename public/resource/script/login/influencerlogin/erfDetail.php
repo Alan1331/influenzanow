eef2a194005fd4dbf371ab91572f00520f29002f
abdulist <dexel7zip@gmail.com>
2022-06-02 23:34:07 +0700

    update dashboard influencer 56%

---
+++ public/resource/script/login/influencerlogin/erfDetail.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html> 
+<html lang="en" style="height:100%;">
+    <head> 
+    <meta charset="utf-8"> 
+        <title>ERF Details</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="keywords" content="pinegrow, blocks, bootstrap" />
+        <meta name="description" content="SIGN UP INFLUENCER" />
+        <link rel="shortcut icon" href="ico/favicon.png"> 
+        <!-- Core CSS -->         
+        <link href="../../../bootstrap/css/bootstrap.min.css" rel="stylesheet"> 
+        <link href="../../../style/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700" rel="stylesheet">
+        <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet">
+        <!-- Style Library -->         
+        <link href="../../../style/css/style-library-1.css" rel="stylesheet">
+        <link href="../../../style/css/plugins.css" rel="stylesheet">
+        <link href="../../../style/css/blocks.css" rel="stylesheet">
+        <link href="../../../style/css/custom.css" rel="stylesheet">
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->         
+        <!--[if lt IE 9]>
+      <script src="../../../style/js/html5shiv.js"></script>
+      <script src="../../../style/js/respond.min.js"></script>
+    <![endif]-->         
+    </head>     
+    <body data-spy="scroll" data-target="nav">
+        <header id="header-1" class="soft-scroll header-1">
+            <!-- Navbar -->
+            <nav class="main-nav navbar-fixed-top headroom headroom--pinned">
+                <div class="container">
+                    <!-- Brand and toggle -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a href="#">
+                            <img src="../../../images/logo-white.png" class="brand-img img-responsive">
+                        </a>
+                    </div>
+                    <!-- Navigation -->
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active nav-item">
+                                <a href="infMenu.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="infProfile.php">Profile</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#">Notification</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a href="#">Message </a> 
+                            </li>
+                            <!--//dropdown-->                     
+                            <li class="nav-item">
+                                <a href="#">Cart</a>
+                            </li>
+                            <li class="nav-item">
+        </li>
+                        </ul>
+                        <!--//nav-->
+                    </div>
+                    <!--// End Navigation -->
+                </div>
+                <!--// End Container -->
+            </nav>
+            <!--// End Navbar -->
+        </header>
+        <section id="content-1-2" class="content-block content-1-2">
+            <div class="container">
+                <!-- Start Row -->
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h2>Task</h2>
+                        <h3>1st ERF</h3>
+                        <p class="lead">Make a Purchase and Promote</p>
+                        <div class="row">
+                            <div class="col-sm-5 col-xs-12">
+                                <a href="erfTask.php" class="btn btn-block btn-warning"><span class="fa fa-check"></span>&nbsp;Apply now</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-5 col-sm-offset-1">
+                        <img class="img-rounded img-responsive" src="../../../images/brands/brand1.png">
+                    </div>
+                </div>
+                <!--// END Row -->
+            </div>
+        </section>
+        <section id="content-2-2" class="content-block content-2-2 bg-pomegranate">
+            <div class="container">
+                <!-- Start Row -->
+                <div class="row">
+                    <div class="col-sm-4 col-xs-12 text-center">
+                        <div class="icon-outline">
+                            <span class="fa fa-shopping-cart"></span>
+                        </div>
+                        <h3>Make a Purchase</h3>
+                        <p>Make a Product Purchase</p>
+                    </div>
+                    <div class="col-sm-4 col-xs-12 text-center">
+                        <div class="icon-outline">
+                            <span class="fa fa-calendar"></span>
+                        </div>
+                        <h3>Purchase Product Deadline</h3>
+                        <p>15 February 2022 - 25 February 2022</p>
+                    </div>
+                    <div class="col-sm-4 col-xs-12 text-center">
+                        <div class="icon-outline">
+                            <span class="fa fa-paper-plane"></span>
+                        </div>
+                        <h3>Promote and Submit Proof</h3>
+                        <p>15 February 2022 - 25 February 2022</p>
+                    </div>
+                </div>
+                <!--// END Row -->
+            </div>
+        </section>
+        <script type="text/javascript" src="../../../style/js/jquery-1.11.1.min.js"></script>         
+        <script type="text/javascript" src="../../../style/js/bootstrap.min.js"></script>         
+        <script type="text/javascript" src="../../../style/js/plugins.js"></script>
+        <script src="https://maps.google.com/maps/api/js?sensor=true"></script>
+        <script type="text/javascript" src="../../../style/js/bskit-scripts.js"></script>           
+    </body>     
+</html>
