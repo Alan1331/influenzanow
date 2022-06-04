898916175f194bf223768f43c13ab5e0f395e186
Alan133 <sahlan.royale@gmail.com>
2022-06-05 00:42:46 +0700

    repairing sign in view

--- public/resource/script/login/influencerlogin/login.php
+++ public/resource/script/login/influencerlogin/login.php
@@ -94,60 +94,105 @@ if( isset($_POST['inf_login']) ) {
     $error = true;
 
 }
+
 ?>
 
-<!DOCTYPE html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-    
-    <style type="text/css">
-
-    #text{
-        height: 25px;
-        border-radius: 5px;
-        padding: 4px;
-        border: solid thin #aaa;
-        width: 100%;
-    }
-    
-    #button{
-        padding: 10px;
-        width: 100px;
-        color: white;
-        background-color: lightblue;
-        border: none;
-    }
-
-    #box{
-        background-color: grey;
-        margin: auto;
-        width: 300px;
-        padding: 20px;
-    }
-
-    </style>
-
-    <div id="box">
-
-        <form method="post" action="">
-            <div style="font-size: 20px;margin: 10px;color: white;">Login</div>
-            <label for="inf_username">Username: </label>
-            <input id="inf_username" type="text" name="inf_username" placeholder="Input your username"><br><br>
-            <label for="inf_password">Password: </label>
-            <input id="inf_password" type="password" name="inf_password" placeholder="Input your password"><br><br>
-            <input type="checkbox" name="remember" id="remember" value="true">
-            <label for="remember">Remember me</label>
-
-            <input id="button" type="submit" name="inf_login" value="login"><br><br>
-
-            <a href="signup.php">Click to Sign up</a>
-            <br><br>
-            <a href="../loginas.php">Back</a>
-        </form>
-
-    </div>
-    
-</body>
+<!DOCTYPE html> 
+<html lang="en" style="height:100%;">
+    <head> 
+    <meta charset="utf-8"> 
+        <title>Task</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="keywords" content="pinegrow, blocks, bootstrap" />
+        <meta name="description" content="SIGN UP INFLUENCER" />
+        <link rel="shortcut icon" href="ico/favicon.png"> 
+        <!-- Core CSS -->         
+        <link href="../../../bootstrap/css/bootstrap.min.css" rel="stylesheet"> 
+        <link href="../../../style/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700" rel="stylesheet">
+        <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet">
+        <!-- Style Library -->         
+        <link href="../../../style/css/style-library-1.css" rel="stylesheet">
+        <link href="../../../style/css/plugins.css" rel="stylesheet">
+        <link href="../../../style/css/blocks.css" rel="stylesheet">
+        <link href="../../../style/css/table.css" rel="stylesheet">
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->         
+        <!--[if lt IE 9]>
+      <script src="../../../style/js/html5shiv.js"></script>
+      <script src="../../../style/js/respond.min.js"></script>
+    <![endif]-->         
+    </head>     
+    <body data-spy="scroll" data-target="nav">
+        <section class="content-block contact-1">
+            <div class="container text-center">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <div class="underlined-title">
+                        <h1>Sign In</h1>
+                        <hr>
+                        <h2>influenza now</h2>
+                    </div>
+                    <div id="contact" class="form-container">
+                        <div id="message"></div>
+                        <form method="post" action="js/contact-form.php" name="contactform" id="contactform">
+                            <!-- /.row -->
+                            <div class="row">
+                                <div class="col-sm-4">
+</div>
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <!-- /.Username -->
+                                        <label for="inf_username">Username</label>
+                                        <input name="inf_username" id="inf_username" type="text" placeholder="Username" class="form-control" />
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+</div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-4">
+</div>
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <!-- /.Password -->
+                                        <label for="inf_password">Password</label>
+                                        <input name="inf_password" id="inf_password" type="password" placeholder="Password" class="form-control" />
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+</div>
+                            </div>
+                            <!-- /.Birthdate -->
+                            <!-- /.Gender -->
+                            
+                                <br>
+                            </label>
+                            <div class="row">
+                            <div>
+                                <input type="checkbox" name="remember" id="remember">
+                                <label for="remember">Remember me</label>
+                            </div>                                 
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <!-- /.Address -->
+                                <p class="small text-muted"><font color="#d90000">Don't have an Account? <a href="signup.php">Register Here</a></font></p>
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-primary" type="submit" id="cf-submit" name="inf_signup">SIGN IN</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.form-container -->
+                </div>
+                <!-- /.col-sm-10 -->
+            </div>
+            <!-- /.container -->
+        </section>
+        <script type="text/javascript" src="../../../style/js/jquery-1.11.1.min.js"></script>         
+    <script type="text/javascript" src="../../../style/js/bootstrap.min.js"></script>         
+    <script type="text/javascript" src="../../../style/js/plugins.js"></script>
+    <script src="https://maps.google.com/maps/api/js?sensor=true"></script>
+    <script type="text/javascript" src="../../../style/js/bskit-scripts.js"></script>        
+    </body>     
 </html>
