c9917cbcfa54b36262b5368cd126134145c03192
Alan133 <sahlan.royale@gmail.com>
2022-06-22 04:20:20 +0700

    create decline application and its notifications

--- public/resource/script/dashboard/influencer/cancelApplyERF.php
+++ public/resource/script/dashboard/influencer/cancelApplyERF.php
@@ -7,6 +7,12 @@ require __DIR__.'/../../../../includes/globalFunctions.php';
 $apply_id = $_GET['apply_id'];
 $inf_id = $_SESSION['inf_id'];
 
+// cek apakah pengajuan sudah ditolak sebelumnya
+$apply_check = mysqli_query($conn, "SELECT apply_id FROM apply_erf WHERE apply_id = $apply_id");
+if( mysqli_num_rows($apply_check) < 1 ) {
+    header('Location: erfDetail.php');
+}
+
 $erf_id = query("SELECT erf_id FROM apply_erf WHERE apply_id = $apply_id")[0]['erf_id'];
 $brand_id = query("SELECT brand_id FROM erf WHERE erf_id = $erf_id")[0]['brand_id'];
 $inf_username = query("SELECT inf_username FROM influencer WHERE inf_id = $inf_id")[0]['inf_username'];
